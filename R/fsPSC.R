2d91ed0a519f1a5b3b30d276740b5d64e6f30d4f
Dmitry Pavlyuk <Dmitry.Pavlyuk@tsi.lv>
2020-03-25 14:10:05 +0000

    version 0.1.4

---
+++ R/fsPSC.R
@@ -0,0 +1,36 @@
+getMaxPSC<-function(mts){
+  k<-ncol(mts)
+  sden<-freqdom::spectral.density(mts)
+  maxPSC <- matrix(0,ncol=k, nrow=k)
+  res <- apply(sden$operators[,,], 3, function(x){
+    S<-solve(x)
+    PSC <- S/(sqrt(diag(S)*diag(S)))
+    v<-(abs(PSC)^2)
+    maxPSC<<-pmax(maxPSC, v)
+    return(list(v))
+  })
+  return(maxPSC)
+}
+fsPSC <- function(mts, max.lag,  show.progress = TRUE, localized = FALSE) {
+  k<-ncol(mts)
+  #res <- simplify2array(res)
+  if (localized){
+    res<-matrix(0, k*max.lag, k)
+    res <- fsNames(res, mts, max.lag)
+    for (i in 1:k){
+      dat <- composeYX(mts, i, max.lag)
+      mPSC<-getMaxPSC(dat)
+      links<-mPSC[1,-1]
+      res[,i] <- links
+      if (show.progress) svMisc::progress(100*i/k)
+    }
+  }else{
+    dat<-mts
+    for (l in 1:max.lag){
+      dat<-cbind(dat[-nrow(dat),], mts[-c(1:l),])
+    }
+    res<-getMaxPSC(dat)[-c(1:k),1:k]
+    res <- fsNames(res, mts, max.lag)
+  }
+  return (res)
+}
